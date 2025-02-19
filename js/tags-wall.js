62057babab4194089a9f208ade1a235db77f1abe
ionfeather <lizishadowmay@gmail.com>
2025-02-19 16:42:55 +0800

    Update

--- js/tags-wall.js
+++ js/tags-wall.js
@@ -131,7 +131,7 @@ const Style1 = (data, marks, $tags, $style, Config) => {
     '93BB6D', '6D9C6D', 'D5E1A1', 'FEF4D8', 'CBE7FD', 'B7CFE2', 'DD8766', 'F6EEE1', 'E88C67', '65A9BE', 'F7C070', '636C8D', 'E2CBC4',
     'BDA995', 'BBD2DB', 'C99B92', '9BB6C7', 'EAB196', 'F0C9AC', 'A2AA71', 'F8F5EE', 'D2CEC1', '8B958A', 'D9DAEE', 'B6BDD9', 'A2C1CB', 
     'BED7DB', 'ABB4AB', 'EADCD0', 'EBA397', 'DEC152', 'CA677C', '9D89AD', 'B5CFE2', 'DDE8EB', 'F7F2EF', 'D2DEDA', '67787F', 'CFD4EE',
-    '#efefef'
+    '#808080'
   ]).map(color => Utils.Color.format(color))
   const FontSize = (size) => size * 5 * Config.scale
 
